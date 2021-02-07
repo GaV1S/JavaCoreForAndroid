fdf86632c4cfdaad11dca703754389ca3eb1fed2
GaV1S <GaV1S@yandex.ru>
2021-02-07 10:09:55 +0300

    Homework6 реализовано через константы у каждого семейства животных.

--- src/main/java/Homework6/Dog.java
+++ src/main/java/Homework6/Dog.java
@@ -2,7 +2,26 @@ package Homework6;
 
 public class Dog extends Animal {
 
-    public Dog(String name, int distanceForRun, int distanceForSwim) {
-        super(name, distanceForRun, distanceForSwim);
+    final int DISTANCE_FOR_RUN = 500;
+    final int DISTANCE_FOR_SWIM = 10;
+
+    public Dog(String name) {
+        super(name);
+    }
+
+    @Override
+    public String run(int distance) {
+        if (distance <= DISTANCE_FOR_RUN)
+            return (super.getName() + " пробежал " + distance + "м");
+        else
+            return (super.getName() + " пробежал " + DISTANCE_FOR_RUN + "м и устал");
+    }
+
+    @Override
+    public String swim(int distance) {
+        if (distance <= DISTANCE_FOR_SWIM)
+            return (super.getName() + " проплыл " + distance + "м");
+        else
+            return (super.getName() + " проплыл " + DISTANCE_FOR_SWIM + "м и устал");
     }
 }
--- src/main/java/Homework6/Main.java
+++ src/main/java/Homework6/Main.java
@@ -5,13 +5,13 @@ public class Main {
     public static void main (String[] args) {
         Cat[] cats = new Cat[2];
 
-        cats[0] = new Cat("Мурзик", 200, 37);
-        cats[1] = new Cat("Нафаня", 150, 0);
+        cats[0] = new Cat("Мурзик");
+        cats[1] = new Cat("Нафаня");
 
         Dog[] dogs = new Dog[2];
 
-        dogs[0] = new Dog("Рада", 500, 10);
-        dogs[1] = new Dog("Мухтар", 150, 21);
+        dogs[0] = new Dog("Рада");
+        dogs[1] = new Dog("Мухтар");
 
         System.out.println(dogs[0].run(600));
         System.out.println(cats[1].swim(100));
